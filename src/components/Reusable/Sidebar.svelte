156e251589147f6bb8d066a4f30bcff1490c934d
AlexanderTheWorst <alexander.backman@elev.ga.ntig.se>
2025-04-07 12:52:42 +0200

    .

---
+++ src/components/Reusable/Sidebar.svelte
@@ -0,0 +1,11 @@
+<main>
+    <section class="details">
+        
+    </section>
+
+    <section class="list">
+        {#each e in [1,2,3,4,5]} 
+            <p>{e}</p>
+        {/each} 
+    </section>
+</main>
