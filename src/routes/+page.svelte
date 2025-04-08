982a048443b821e99213efaabcf042601799084b
AlexanderTheWorst <alexander.backman@elev.ga.ntig.se>
2025-04-08 10:47:07 +0200

    Idk bro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script>
     import Sidebar from "@components/core/Sidebar.svelte";
 
+    import { writable } from "svelte/store";
+
+    let app_data = writable({
+        current_page: "server_details",
+    });
+
     let current_page;
     const pages = import.meta.glob("@components/pages/*.svelte");
     $: if ($app_data.current_page) {
@@ -10,32 +16,24 @@
 
         if (target) pages[target]().then((mod) => (current_page = mod.default));
         else {
-            target = Object.keys(pages).find((path) =>
-                path.toLowerCase().includes(`404.svelte`),
+            import("@components/pages/404.svelte").then(
+                (mod) => (current_page = mod.default),
             );
-            pages[target]().then((mod) => (current_page = mod.default));
-            console.warn("Error! Target page does not exist in src/Pages");
         }
     } else {
         current_page = undefined;
     }
-
-    import { writable } from "svelte/store";
-
-    let app_data = writable({
-        current_page: "system_details",
-    });
 </script>
 
 <div class="w-screen min-h-screen bg-primary flex">
     <Sidebar {app_data} />
     <section class="container w-full min-h-screen flex flex-col">
-        <h2>{$app_data.current_page}</h2>
         {#if current_page}
-            <svelte:component this={current_page} app_data={app_data}; class="flex-1" />
+            <svelte:component
+                this={current_page}
+                app_data="{app_data};"
+                class="flex-1"
+            />
         {/if}
-        <!-- {#each Object.keys(loaded_pages) as page}
-            <svelte:component this={loaded_pages[page]} />
-        {/each} -->
     </section>
 </div>
